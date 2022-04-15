35bdaf8ae5c0fe2420d665a6ceb5200051d82315
zishan344 <zishanahmed344@gmail.com>
2022-04-15 22:13:56 +0600

    hero section design done:

--- src/Pages/Home/HeroSection/HeorSection.js
+++ src/Pages/Home/HeroSection/HeorSection.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import "./HeroSection.css";
 const HeorSection = () => {
   return (
-    <div>
-      <h1>This is hero section</h1>
+    <div className="hero flex justify-center items-center">
+      <div>
+        <h1 className="text-5xl font-semibold mb-6">
+          Best Food waiting for your belly
+        </h1>
+        <div className="text-center relative ">
+          <input
+            className="w-4/5 outline-none  h-12 rounded-full px-4 text-center "
+            type="text"
+            name=""
+            id=""
+            placeholder="search food items"
+          />
+          <button className="absolute  px-8 right-0 py-3 bg-red-600 rounded-full text-1xl font-semibold text-white hover:bg-red-500">
+            Search
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Sheard/Header/Header.js
+++ src/Sheard/Header/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
 const Header = () => {
   return (
     <div>
-      <h2>Thsi is Header section</h2>
+      <nav className="flex items-center justify-between max-w-6xl mx-auto py-3">
+        <div>
+          <img style={{ height: "50px" }} src={logo} alt="" />
+        </div>
+        <div className="flex items-center gap-6">
+          <Link to="/addedCart">
+            <BsFillCartCheckFill />
+          </Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">
+            <button className="px-4 py-1 bg-red-600 rounded-full text-1xl font-semibold text-white hover:bg-red-500">
+              Register
+            </button>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
